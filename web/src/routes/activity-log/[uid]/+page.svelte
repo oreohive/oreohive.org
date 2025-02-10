7a5b8ea571c91c7c95f57b294fd20a33753b9030
opreo <77515999+dopreo@users.noreply.github.com>
2025-02-10 18:08:17 +0000

    feat: initialise project structure and add Prismic integration

---
+++ web/src/routes/activity-log/[uid]/+page.svelte
@@ -0,0 +1,12 @@
+<!-- src/routes/activity-log/[uid]/+page.svelte -->
+
+<script>
+        import { SliceZone } from '@prismicio/svelte';
+
+        import { components } from '$lib/slices';
+        import { page } from '$app/state';
+
+        export let data;
+</script>
+
+<SliceZone slices={data.page.data.slices} {components} />
