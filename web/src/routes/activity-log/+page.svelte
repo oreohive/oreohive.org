037d804dfa8f7986f34d3b86a1d3d3d5fae06ea8
opreo <77515999+dopreo@users.noreply.github.com>
2025-02-11 21:41:53 +0000

    quick fix: add hrefs to panels properly now they can accept them as passed

--- web/src/routes/activity-log/+page.svelte
+++ web/src/routes/activity-log/+page.svelte
@@ -13,7 +13,7 @@ export let data;
 <div>
 	<li style="display: flex; flex-direction: row; align-items: center; margin-bottom: 16px; gap: 32px;">
         <a href={`/activity-log/${post.uid}`}>
-            <Panel colour="#fcfcff" desc={post.data.title} img_url={post.data.image.url} />
+            <Panel colour="#fcfcff" desc={post.data.title} img_url={post.data.image.url} href={`/activity-log/${post.uid}`}/>
         </a>
 	<p>{post.data.date}</p>
 	<p>{post.data.author}</p>
