6b69552db65632f5b82345fc3cb3a6539e3ad667
dopreo <77515999+dopreo@users.noreply.github.com>
2025-02-12 12:16:15 +0000

    FIXED RICH TEXT FORMATTING WOOOOO

--- web/src/routes/activity-log/[uid]/+page.svelte
+++ web/src/routes/activity-log/[uid]/+page.svelte
@@ -1,7 +1,7 @@
 <!-- src/routes/activity-log/[uid]/+page.svelte -->
 
 <script lang="ts">
-  import { SliceZone } from '@prismicio/svelte';
+  import { PrismicEmbed, PrismicImage, PrismicLink, PrismicRichText, PrismicText, SliceZone } from '@prismicio/svelte';
   import { components } from '$lib/slices';
   import { page } from '$app/state';
 
@@ -11,41 +11,15 @@
 <h1>oreohive activity log</h1>
 
 {#if data.page}
-  <h2>{data.page.data.title}</h2>
+  <h1>{data.page.data.title}</h1>
   <p>{data.page.data.date} | {data.page.data.author}</p>
   <p>{data.page.data.description}</p>
 
-  <img src={data.page.data.image.url} alt={data.page.data.image.alt} style="width: 512px; height: 512px;">
+  <PrismicImage field={data.page.data.image} />
 
-  <div class="text-box">
-  {#each data.page.data.post_content as content} <!-- may switch save us lmao -->
-    {#if content.type === 'paragraph'}
-      <p>{@html content.text}</p>
-    {/if}
-    {#if content.type === 'heading1'}
-      <h1>{@html content.text}</h1>
-    {/if}
-    {#if content.type === 'heading2'}
-      <h2>{@html content.text}</h2>
-    {/if}
-    {#if content.type === 'heading3'}
-      <h3>{@html content.text}</h3>
-    {/if}
-    {#if content.type === 'heading4'}
-      <h4>{@html content.text}</h4>
-    {/if}
-    {#if content.type === 'heading5'}
-      <h5>{@html content.text}</h5>
-    {/if}
-    {#if content.type === 'heading6'}
-      <h6>{@html content.text}</h6>
-    {/if}
-    {#if content.type === 'image'}
-      <img src={content.url} alt={content.alt} style="width: 512px; height: 512px;">
-    {/if}
-  {/each}
+<div class="text-box">
+  <PrismicRichText field={data.page.data.post_content} />
 </div>
-
   <SliceZone slices={data.page.data.slices} {components} />
 {/if}
 
