09a80d3cb3053e996e06d1992df41ea2bb33a364
dopreo <77515999+dopreo@users.noreply.github.com>
2025-02-14 14:24:02 +0000

    fix that fixes post displays; previous commit and push broke the activity log lol

--- web/src/routes/activity-log/+page.server.ts
+++ web/src/routes/activity-log/+page.server.ts
@@ -7,9 +7,15 @@ export const prerender = false;
 export async function load({ fetch, cookies }) {
   const client = createClient({ fetch, cookies });
 
-  const posts = await client.getAllByType("post", {orderings: "[my.post.date desc]"});
+  const posts = await client.getAllByType("post", {
+    
+    orderings: [
+      { field: "my.post.date", direction: "desc" }
+    ]
+  
+  });
 
-  console.log("posts:", posts); // temp: to inspect posts object
+  console.log("posts:", posts); // can be considered temp: to inspect posts object
 
   return {
     posts,
