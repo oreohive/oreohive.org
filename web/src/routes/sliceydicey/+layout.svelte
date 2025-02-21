f9102e478fbacf0a24b9ffd0497eb896ce08e8b2
dopreo <77515999+dopreo@users.noreply.github.com>
2025-02-21 23:51:13 +0000

    hotfix for mobile again lol; had set a min-width to like 540px or something

--- web/src/routes/sliceydicey/+layout.svelte
+++ web/src/routes/sliceydicey/+layout.svelte
@@ -1,5 +1,5 @@
 <div class="container" style="min-height: 94vh;">
-    <div class="centre-container" style="min-width: 540px; padding: 64px;">
-        <slot />
-    </div>
+  <div class="centre-container" style="min-width: 28.2435vw; padding: 64px;"> <!-- "674" w -->
+    <slot />
+  </div>
 </div>
