a851c4db9a880ebdabdbc555f97c81ce642c64b1
opreo <77515999+dopreo@users.noreply.github.com>
2025-02-23 19:47:07 +0000

    actually fill in examples lol

--- web/src/components/SEO.svelte
+++ web/src/components/SEO.svelte
@@ -3,33 +3,24 @@
 <script>
   import { page } from "$app/stores";
 
-  export let title = "Home | Example.com";
-  export let description = "Description of your website.";
-  export let image = "https://example.com/your-logo.png";
+  export let title = "oreohive.org";
+  export let description = "let's make a better internet, together.";
+  export let image = "https://www.oreohive.org/logo.webp";
 </script>
 
 <svelte:head>
-  <title>{title} | Example.com</title>
+  <title>oreohive.org | {title}</title>
   <meta name="description" content={description} />
-  <meta property="og:site_name" content="Example.com" />
+  <meta property="og:site_name" content="oreohive.org" />
   <meta
     property="og:url"
-    content="https://www.example.com{$page.url.pathname.toString()}"
+    content="https://www.oreohive.org{$page.url.pathname.toString()}"
   />
   <meta property="og:type" content="website" />
   <meta property="og:title" content={title} />
   <meta property="og:description" content={description} />
   <meta property="og:image" content={image} />
-
-  <meta name="twitter:card" content="summary_large_image" />
-  <meta property="twitter:domain" content="example.com" />
-  <meta
-    property="twitter:url"
-    content="https://www.example.com{$page.url.pathname.toString()}"
-  />
-  <meta name="twitter:title" content={title} />
-  <meta name="twitter:description" content={description} />
-  <meta name="twitter:image" content={image} />
+>
   {@html `
       <script type="application/ld+json">{
         "@context": "https://schema.org",
