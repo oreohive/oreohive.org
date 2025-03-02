baac4da163a86c669d3cc684805c97e90644dc61
opreo <77515999+dopreo@users.noreply.github.com>
2025-03-02 18:54:34 +0000

    remove image constraints in article types, set up oreohive labs log

---
+++ web/src/routes/labs/+page.server.ts
@@ -0,0 +1,23 @@
+// src/routes/labs/+page.server.ts
+
+import { createClient } from "$lib/prismicio";
+
+export const prerender = false;
+
+export async function load({ fetch, cookies }) {
+  const client = createClient({ fetch, cookies });
+
+  const posts = await client.getAllByType("labsArticle", {
+    
+    orderings: [
+      { field: "my.post.timestamp", direction: "desc" }
+    ]
+  
+  });
+
+  console.log("posts:", posts); // can be considered temp: to inspect posts object
+
+  return {
+    posts,
+  };
+}
