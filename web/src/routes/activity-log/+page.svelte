f8468eb45fdb01f0cded6916e92e928be287eaba
dopreo <77515999+dopreo@users.noreply.github.com>
2025-03-11 21:47:41 +0000

    hotfix: make titles legible in dark mode

--- web/src/routes/activity-log/+page.svelte
+++ web/src/routes/activity-log/+page.svelte
@@ -11,7 +11,7 @@ export let data;
 <ul>
   <div style="display: flex; flex-wrap: wrap;">
   {#each [...data.posts] as post} <!-- ok it seems to not order with date by default, at least not properly, so uhh let's do this ourselves lol (?) -->
-    <li style="height: 324x; width: 300px; margin: 20px; border-style: dashed; background-color:hsl(340, 22%, 95%);">
+    <li style="height: 324x; width: 300px; margin: 20px; border-style: dashed; background-color:rgb(245, 239, 241, 0.05);">
       <Panel colour="#fcfcff" img_url={post.data.image.url ?? "/logo.webp"} href={`/activity-log/${post.uid}`}/>
       <div>
         <a href={`/activity-log/${post.uid}`}><h3>{post.data.title}</h3></a>
