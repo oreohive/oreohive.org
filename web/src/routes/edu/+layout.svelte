61df71103cb753a9c9e9bd794abab1bea539eef8
dopreo <77515999+dopreo@users.noreply.github.com>
2025-07-10 09:58:55 +0100

    refine wip warning at /edu

--- web/src/routes/edu/+layout.svelte
+++ web/src/routes/edu/+layout.svelte
@@ -1,24 +1,18 @@
 <script>
 </script>
 
-<p style="font-size: 0.9em; margin: 6px; max-width: 50vw; border-width: 1.75px; border-style: dashed; border-color: #ff0059; color: #e00064;">
+<p style="font-size: 0.55em; margin: 6px; max-width: 50vw; border-width: 1.5px; border-style: dashed; border-color: #ff0059; color: #e00064;">
     edu IS A <b>HUIGE WORK IN PROGRESS</b> - stay away if you're allergic to half-finished websites.<br>
-    check out our GitHub and clone our repo if
-    you wanna contribute!<br />
-    in the meantime, though, you'll find a ton of <b>broken (404 not found) links</b>. please bear
-    with us.<br>
-    you should also probably see a doctor about that half-finished websites thing.
+    oyu'll find a ton of <b>broken (404 not found) links</b>. please bear
+    with us or contribute on our github!.<br>
 </p>
 
 <slot />
 
-<div class="container" style="min-height: 76vh; gap: 0px; padding: 0px; height: 24px; min-height: 25vh;">
-    <div class="footnote" style="font-size: 0.85em;">
-        <b>edu on oreohive.org is ridiculously incomplete</b>, and <b>all that resides here for now is pretty much proof-of-concept</b>.
-        if you're looking for <b>legitimate</b>, <b>fleshed-out</b> revision resources, i personally recommend <a href="https://senecalearning.com"><b>seneca learning</b></a>.<br>
-    </div>
-    <div class="footnote" style="font-size: 0.75em;">
-        <i>this is <b>not sponsored</b>, nor is this a paid-for product placement. we, at present, have no ties or affiliations with seneca learning or its parent entity.</i>
+<div class="container" style="gap: 0px; padding: 0px; height: 48px;">
+    <div style="font-size: 0.85em;">
+        <b>oreohive edu is very incomplete</b>!
+        if you're looking for <b>legitimate</b> revision resources, maybe try <a href="https://senecalearning.com"><b>seneca learning</b></a>.<br>
     </div>
 </div>
 
